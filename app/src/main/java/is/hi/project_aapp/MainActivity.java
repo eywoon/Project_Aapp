211054e1dcc4a9cd4e9455b3a4ee618e3278808d
Hrefna <hrefnanana@gmail.com>
2016-04-10 15:08:11 +0000

    mainactivity lagað

--- app/src/main/java/is/hi/project_aapp/MainActivity.java
+++ app/src/main/java/is/hi/project_aapp/MainActivity.java
@@ -1,21 +1,27 @@
 package is.hi.project_aapp;
 
 import android.content.Intent;
+import android.content.SyncAdapterType;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.Calendar;
+import org.joda.time.LocalDate;
 
 import is.hi.project_aapp.Emotions.Emotions;
-//import is.hi.project_aapp.GoalsActivity.GoalsActivity;
-//import is.hi.project_aapp.Help.Help;
-//import is.hi.project_aapp.MeActivity.MeActivity;
-//import is.hi.project_aapp.Results.Results;
-//import is.hi.project_aapp.Settings.Settings;
 import is.hi.project_aapp.Goals.GoalsActivity;
 import is.hi.project_aapp.Help.Help;
 import is.hi.project_aapp.Me.MeActivity;
+import is.hi.project_aapp.Me.User;
 import is.hi.project_aapp.Results.Results;
+import is.hi.project_aapp.SQL.AAppDatabaseHelper;
 import is.hi.project_aapp.Settings.Settings;
 import is.hi.project_aapp.Steps.StepsActivity;
 import is.hi.project_aapp.TaskManager.TaskActivity;
@@ -30,6 +36,72 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    protected void onResume()
+    {
+        super.onResume();
+        updateView();
+    }
+
+    public void updateView(){
+        //Create a cursor
+        try {
+            SQLiteOpenHelper aappDatabaseHelper = new AAppDatabaseHelper(this);
+            SQLiteDatabase db = aappDatabaseHelper.getReadableDatabase();
+            Cursor cursor = db.query("USER",
+                    new String[]{"FIRSTNAME", "SOBERYEAR", "SOBERMONTH", "SOBERDAY"},
+                    null, null, null, null, null);
+            if(cursor.moveToFirst()){
+                //Get the drink details from the cursor
+                String firstNameText = cursor.getString(0);
+                int soberYearText = cursor.getInt(1);
+                int soberMonthText = cursor.getInt(2);
+                int soberDayText = cursor.getInt(3);
+
+                LocalDate dateToday = LocalDate.now();
+
+                String soberDate = "";
+                dateToday = dateToday.minusYears(soberYearText);
+                dateToday = dateToday.minusMonths(soberMonthText);
+                dateToday = dateToday.minusDays(soberDayText);
+
+
+
+                if(dateToday.getYear() > 0){
+                    soberDate =  dateToday.getYear()+" ár ";
+                }
+                if (dateToday.getMonthOfYear() > 1){
+                    soberDate = soberDate+dateToday.getMonthOfYear() + " mánuði ";
+                }
+                if (dateToday.getMonthOfYear() == 1){
+                    soberDate = soberDate+dateToday.getMonthOfYear() + " mánuð ";
+                }
+
+                if (dateToday.getDayOfMonth() == 1){
+                    soberDate = soberDate+dateToday.getDayOfMonth() + " dag";
+                }
+                if (dateToday.getDayOfMonth() > 1){
+                    soberDate = soberDate+dateToday.getDayOfMonth() + " daga";
+                }
+
+                TextView name = (TextView)findViewById(R.id.name);
+                name.setText(firstNameText);
+                TextView soberDateView = (TextView)findViewById(R.id.dagar);
+                soberDateView.setText(soberDate);
+            }
+            cursor.close();
+            db.close();
+        }
+        catch (SQLiteException e){
+            Toast toast = Toast.makeText(this, "VESEN", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+    }
+
+
+
+
 
     public void on12stepsClick (View view){
         Intent intent = new Intent(this, StepsActivity.class);
